c99d898243722ffa10d4158902cc7d757a975cdf
Jan Segre <jan@segre.in>
2012-06-03 21:28:47 -0300

    Separating WinMain to its own .hpp so it can be reused.

--- src/graphicalClient/main.cpp
+++ src/graphicalClient/main.cpp
@@ -24,6 +24,7 @@
 #include <QApplication>
 #include "soccerview.h"
 #include "timer.h"
+#include "winmain.hpp"
 
 GLSoccerView *view;
 
@@ -70,31 +71,4 @@ int main(int argc, char **argv)
   return retVal;
 }
 
-#ifdef HAVE_WINDOWS
-#include <QMainWindow>
-
-int CALLBACK WinMain(
-    __in  HINSTANCE hInstance,
-    __in  HINSTANCE hPrevInstance,
-    __in  LPSTR lpCmdLine,
-    __in  int nCmdShow)
-{
-    wchar_t **wargv;//wargv is an array of LPWSTR (wide-char string)
-    char **argv;
-    int argc;
-    // we have to convert each arg to a char*
-    wargv = CommandLineToArgvW(GetCommandLineW(), &argc);
-    // here and then we allocate one more cell to make it a NULL-end array
-    argv = (char **)calloc(argc + 1, sizeof(char *));
-    for(int i = 0; i < argc; i++) {
-        size_t origSize = wcslen(wargv[i]) + 1;
-        size_t converted = 0;
-        argv[i] = (char *)calloc(origSize + 1, sizeof(char));
-        // we use the truncate strategy, it won't handle non latin chars correctly
-        // but hey we don't even use args, right?
-        wcstombs_s(&converted, argv[i], origSize, wargv[i], _TRUNCATE);
-    }
-    return main(argc, argv);
-}
-#endif
 
